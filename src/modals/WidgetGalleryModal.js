85be1c135527aee38e5412f0707e16d0ccf30a57
Devangam Sajja Rajesh <104376917+Rajeshds20@users.noreply.github.com>
2023-06-29 22:44:29 +0530

    Added and Updated

--- src/modals/WidgetGalleryModal.js
+++ src/modals/WidgetGalleryModal.js
@@ -3,10 +3,12 @@ import ClockWidget from '../widgets/ClockWidget'
 import ReminderListWidget from '../widgets/ReminderListWidget'
 import TimerWidget from '../widgets/TimerWidget'
 import CalendarWidget from '../widgets/CalendarWidget'
+import MyCustomWidget from '../widgets/MyCustomWidget'
 
 export default function WidgetGalleryModal({ setShowWidgetModal, selectedWidgetArea, widgets, setWidgets }) {
     const [galleryWidgets, setGalleryWidgets] = useState([
         { component: <ClockWidget />, name: "Date and Time" },
+        { component: <MyCustomWidget />, name: "Weather" },
         { component: <ReminderListWidget />, name: "Reminder List" },
         { component: <TimerWidget />, name: "Timer" },
         { component: <CalendarWidget />, name: "Calendar" },
