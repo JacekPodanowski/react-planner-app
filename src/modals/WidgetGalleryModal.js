7c6a3732cce25445d46764c4045a4dcdef5f3b27
Jacek Podanowski <jacpodski@gmail.com>
2025-03-04 14:39:30 +0100

    deleted weather wiget

--- src/modals/WidgetGalleryModal.js
+++ src/modals/WidgetGalleryModal.js
@@ -3,12 +3,10 @@ import ClockWidget from '../widgets/ClockWidget'
 import ReminderListWidget from '../widgets/ReminderListWidget'
 import TimerWidget from '../widgets/TimerWidget'
 import CalendarWidget from '../widgets/CalendarWidget'
-import MyCustomWidget from '../widgets/MyCustomWidget'
 
 export default function WidgetGalleryModal({ setShowWidgetModal, selectedWidgetArea, widgets, setWidgets }) {
     const [galleryWidgets, setGalleryWidgets] = useState([
         { component: <ClockWidget />, name: "Date and Time" },
-        { component: <MyCustomWidget />, name: "Weather" },
         { component: <ReminderListWidget />, name: "Reminder List" },
         { component: <TimerWidget />, name: "Timer" },
         { component: <CalendarWidget />, name: "Calendar" },
